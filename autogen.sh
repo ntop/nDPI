3caf906c9a7cf87a72225994ae404fa2e384b16e
Luca Deri <deri@ntop.org>
2015-05-23 07:15:24 +0200

    Fixes to catch missing GNU tools

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,30 @@
 
 
 /bin/rm -f configure config.h config.h.in src/lib/Makefile.in
+
+AUTOCONF=$(which autoconf)
+AUTOMAKE=$(which automake)
+LIBTOOL=$(which libtool)
+
+if test -z $AUTOCONF; then
+    echo "autoconf is missing: please install it and try again"
+    exit
+fi
+
+if test -z $AUTOMAKE; then
+    echo "automake is missing: please install it and try again"
+    exit
+fi
+
+if test -z $LIBTOOL; then
+    echo "libtool is missing: please install it and try again"
+    exit
+fi
+
+if test -z $AUTORECONF; then
+    echo "autoreconf is missing: please install it and try again"
+    exit
+fi
+
 autoreconf -ivf
 ./configure
