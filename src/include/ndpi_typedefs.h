984eca8a27346314e7826522f955b37148b79dde
Luca Deri <deri@ntop.org>
2015-06-15 00:47:39 +0200

    Added support for Twitch detection Minor cosmetic fix in test tool

--- src/include/ndpi_typedefs.h
+++ src/include/ndpi_typedefs.h
@@ -618,8 +618,10 @@ typedef struct ndpi_detection_module_struct {
   u_int ndpi_num_custom_protocols;
 
   /* HTTP/DNS/HTTPS host matching */
-  ndpi_automa host_automa, content_automa, bigrams_automa, impossible_bigrams_automa;
-
+  ndpi_automa host_automa, /* Used for DNS/HTTPS */
+    content_automa,  /* Used for HTTP subprotocol_detection */
+    subprotocol_automa, /* Used for HTTP subprotocol_detection */
+    bigrams_automa, impossible_bigrams_automa; /* TOR */
   /* IP-based protocol detection */
   void *protocols_ptree;
 
