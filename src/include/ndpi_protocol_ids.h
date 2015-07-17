eb49f15453837483e6966768a437ae7c9ea4100d
Matteo Bracci <matteobracci1@gmail.com>
2015-07-17 18:47:00 +0200

    Update ndpi_protocol_ids.h

--- src/include/ndpi_protocol_ids.h
+++ src/include/ndpi_protocol_ids.h
@@ -199,6 +199,7 @@
 #define NDPI_PROTOCOL_TELEGRAM				        185 /* Gianluca Costa <g.costa@xplico.org> */
 #define NDPI_PROTOCOL_QUIC				        188 /* Andrea Buscarinu <andrea.buscarinu@gmail.com> - Michele Campus <michelecampus5@gmail.com> */
 #define NDPI_PROTOCOL_WHATSAPP_VOICE			        189
+#define NDPI_PROTOCOL_STARCRAFT2			213   /* matteobracci1@gmail.com */
 
 
 #define NDPI_CONTENT_AVI				39
@@ -261,7 +262,7 @@
 #define NDPI_SERVICE_MICROSOFT                          212
 
 /* UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE */
-#define NDPI_LAST_IMPLEMENTED_PROTOCOL			NDPI_SERVICE_MICROSOFT
+#define NDPI_LAST_IMPLEMENTED_PROTOCOL			NDPI_PROTOCOL_STARCRAFT2
 
 #define NDPI_MAX_SUPPORTED_PROTOCOLS                    (NDPI_LAST_IMPLEMENTED_PROTOCOL + 1)
 #define NDPI_MAX_NUM_CUSTOM_PROTOCOLS                   (NDPI_NUM_BITS-NDPI_LAST_IMPLEMENTED_PROTOCOL)
