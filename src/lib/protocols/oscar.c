3e5033ff13225743ee29e5f899f1f72710a04dda
theirix <theirix@gmail.com>
2016-07-25 17:44:38 +0300

    Fail when Oscar family is 0

--- src/lib/protocols/oscar.c
+++ src/lib/protocols/oscar.c
@@ -245,10 +245,10 @@ static void ndpi_search_oscar_tcp_connect(struct ndpi_detection_module_struct
       if (channel == DATA)
 	{
 	  if (packet->payload_packet_len >= 8)
-	  {
 	    family = get_u_int16_t(packet->payload, 6);
-	  }
 	  else
+	    family = 0;
+	  if (family == 0)
 	  {
 	      NDPI_ADD_PROTOCOL_TO_BITMASK(flow->excluded_protocol_bitmask, NDPI_PROTOCOL_OSCAR);
 	      return;
