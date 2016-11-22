797eb2c3ca69654c5e1926f626882dcd9a5a1c0b
Campus <campus@ntop.org>
2016-11-22 23:11:24 +0100

    fix initialization of struct ndpi_flow_info *flow

--- example/ndpi_util.c
+++ example/ndpi_util.c
@@ -446,7 +446,7 @@ static unsigned int packet_processing(struct ndpi_workflow * workflow,
 				      u_int16_t ip_offset,
 				      u_int16_t ipsize, u_int16_t rawsize) {
   struct ndpi_id_struct *src, *dst;
-  struct ndpi_flow_info *flow;
+  struct ndpi_flow_info *flow = NULL;
   struct ndpi_flow_struct *ndpi_flow = NULL;
   u_int8_t proto;
   struct ndpi_tcphdr *tcph = NULL;
