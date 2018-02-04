26e36a19bf5405a453569d47ea9439a1e4c09581
ZengYingpei <zengyingpei@cmhi.chinamobile.com>
2018-02-04 14:35:56 +0800

    Unify Copyright declaration for all added files of the Xcode project

--- example/MacOS/ndpiExample/ndpiExample/ndpi_utils.h
+++ example/MacOS/ndpiExample/ndpiExample/ndpi_utils.h
@@ -1,10 +1,24 @@
-//
-//  ndpi_utils.h
-//  ndpiExample
-//
-//  Created by ZengYingpei on 2018/2/4.
-//  Copyright © 2018年 ZengYingpei. All rights reserved.
-//
+/*
+ *
+ * Copyright (C) 2011-18 - ntop.org
+ *
+ * This file is part of nDPI, an open source deep packet inspection
+ * library based on the OpenDPI and PACE technology by ipoque GmbH
+ *
+ * nDPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * nDPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with nDPI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef ndpi_utils_h
 #define ndpi_utils_h
