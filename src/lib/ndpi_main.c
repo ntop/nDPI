df54e8b926dcb4ebb3f4bae49197c2d5988464ee
Luca Deri <deri@ntop.org>
2018-02-12 21:44:48 +0100

    Improved numeric to protocol conversion

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -4718,9 +4718,13 @@ char* ndpi_protocol2name(struct ndpi_detection_module_struct *ndpi_mod,
 			 ndpi_protocol proto, char *buf, u_int buf_len) {
   if((proto.master_protocol != NDPI_PROTOCOL_UNKNOWN)
      && (proto.master_protocol != proto.app_protocol)) {
-    snprintf(buf, buf_len, "%s.%s",
-	     ndpi_get_proto_name(ndpi_mod, proto.master_protocol),
-	     ndpi_get_proto_name(ndpi_mod, proto.app_protocol));
+    if(proto.app_protocol != NDPI_PROTOCOL_UNKNOWN)
+      snprintf(buf, buf_len, "%s.%s",
+	       ndpi_get_proto_name(ndpi_mod, proto.master_protocol),
+	       ndpi_get_proto_name(ndpi_mod, proto.app_protocol));
+    else
+      snprintf(buf, buf_len, "%s",
+	       ndpi_get_proto_name(ndpi_mod, proto.master_protocol));
   } else
     snprintf(buf, buf_len, "%s",
 	     ndpi_get_proto_name(ndpi_mod, proto.app_protocol));
