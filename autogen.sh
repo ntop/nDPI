0e11abc77fb8b2b969a19e341261394184ad4b4c
Simone Mainardi <mainardi@ntop.org>
2018-04-06 17:23:12 +0200

    Bump to version 2.2.2

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 NDPI_MAJOR="2"
-NDPI_MINOR="3"
-NDPI_PATCH="0"
+NDPI_MINOR="2"
+NDPI_PATCH="2"
 NDPI_VERSION_SHORT="$NDPI_MAJOR.$NDPI_MINOR.$NDPI_PATCH"
 
 rm -f configure config.h config.h.in src/lib/Makefile.in
