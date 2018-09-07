76b2d29da2b23612cf91a00f48f9d254ac05a0ce
Kumar Kandasamy <kumar.kandasamy@tarento.com>
2018-09-07 23:21:36 +0530

    Adding Entertainment and Video Portals protocols to Configuration

--- src/include/ndpi_protocol_ids.h
+++ src/include/ndpi_protocol_ids.h
@@ -279,6 +279,9 @@ typedef enum {
   NDPI_PROTOCOL_AMAZON_VIDEO          = 240,
   NDPI_PROTOCOL_GOOGLE_DOCS           = 241,
   NDPI_PROTOCOL_WHATSAPP_FILES        = 242, /* Videos, pictures, voice messages... */
+  NDPI_PROTOCOL_SNAPDEAL              = 243, /* Kumar Kandasamy <kumarks1122@gmail.com> */
+  NDPI_PROTOCOL_FLIPKART              = 244, /* Kumar Kandasamy <kumarks1122@gmail.com> */
+  NDPI_PROTOCOL_HOTSTAR               = 245, /* Kumar Kandasamy <kumarks1122@gmail.com> */
   /*
     IMPORTANT
     before allocating a new identifier please fill up
