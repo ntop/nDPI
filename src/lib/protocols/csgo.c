7415af5086121c6196ad1dc9d0232b000f869ac0
Derrick Lyndon Pallas <derrick@argosylabs.com>
2019-04-06 00:29:00 +0000

    csgo: fix invalid read on short packet

--- src/lib/protocols/csgo.c
+++ src/lib/protocols/csgo.c
@@ -30,6 +30,11 @@ void ndpi_search_csgo(struct ndpi_detection_module_struct* ndpi_struct, struct n
   struct ndpi_packet_struct* packet = &flow->packet;
 
   if (packet->udp != NULL) {
+    if (packet->payload_packet_len < sizeof(uint32_t)) {
+      NDPI_LOG_DBG2(ndpi_struct, "Short csgo packet\n");
+      return;
+    }
+
     uint32_t w = htonl(get_u_int32_t(packet->payload, 0));
     NDPI_LOG_DBG2(ndpi_struct, "CSGO: word %08x\n", w);
 
