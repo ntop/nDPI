8186b72c11acf16904460613ad45ed942d6f6e59
Alfredo Cardigliano <alfredo.cardigliano@gmail.com>
2019-09-10 19:30:52 +0200

    Added ndpi_deserialize_clone_all to the public API

--- src/include/ndpi_api.h
+++ src/include/ndpi_api.h
@@ -882,6 +882,7 @@ extern "C" {
   int ndpi_deserialize_value_string(ndpi_deserializer *deserializer, ndpi_string *value);
 
   int ndpi_deserialize_clone_item(ndpi_deserializer *deserializer, ndpi_serializer *serializer);
+  int ndpi_deserialize_clone_all(ndpi_deserializer *deserializer, ndpi_serializer *serializer);
 
   /* Data analysis */
   struct ndpi_analyze_struct* ndpi_alloc_data_analysis(u_int16_t _max_series_len);
