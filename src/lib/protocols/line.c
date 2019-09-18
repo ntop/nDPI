487110693c3b737bbb8d0995bb4cdb14ebd0015e
Luca Deri <deri@ntop.org>
2019-09-18 23:23:13 +0200

    Line protocol update

--- src/lib/protocols/line.c
+++ src/lib/protocols/line.c
@@ -38,17 +38,17 @@ void ndpi_search_line(struct ndpi_detection_module_struct *ndpi_struct, struct n
 
   if(packet->iph) {
     /* 125.209.252.xxx */
-    if(((ntohl(packet->iph->saddr) & 0xFFFFFF00 /* 255.255.255.0 */) == 0x7DD1FC00)
-       || ((ntohl(packet->iph->daddr) & 0xFFFFFF00 /* 255.255.255.0 */) == 0x7DD1FC00)) {
-      if((packet->payload_packet_len == 110)
-	 && (flow->packet.payload[0] == 0xB6) && (flow->packet.payload[1] == 0x18)
-	 && (flow->packet.payload[2] == 0x00) && (flow->packet.payload[3] == 0x6A)) {
+    if (((ntohl(packet->iph->saddr) & 0xFFFFFF00 /* 255.255.255.0 */) == 0x7DD1FC00) ||
+	((ntohl(packet->iph->daddr) & 0xFFFFFF00 /* 255.255.255.0 */) == 0x7DD1FC00)) {
+      if ((packet->payload_packet_len == 110) && (flow->packet.payload[0] == 0xB6) &&
+	  (flow->packet.payload[1] == 0x18) && (flow->packet.payload[2] == 0x00) &&
+	  (flow->packet.payload[3] == 0x6A)) {
 	ndpi_line_report_protocol(ndpi_struct, flow);
 	return;
       }
     }
   }
-
+  
   NDPI_EXCLUDE_PROTO(ndpi_struct, flow);
 }
 
