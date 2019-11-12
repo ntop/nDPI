ab9ccf9eb9a6571d84bd0b2c8e185565d0d700f6
Luca Deri <deri@ntop.org>
2019-11-12 12:28:34 +0100

    Fixed new nDPi API

--- fuzz/fuzz_process_packet.c
+++ fuzz/fuzz_process_packet.c
@@ -9,20 +9,20 @@ struct ndpi_id_struct *src;
 struct ndpi_id_struct *dst;
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-    if (ndpi_info_mod == NULL) {
-        ndpi_info_mod = ndpi_init_detection_module();
-        NDPI_PROTOCOL_BITMASK all;
-        NDPI_BITMASK_SET_ALL(all);
-        ndpi_set_protocol_detection_bitmask2(ndpi_info_mod, &all);
-        ndpi_flow = ndpi_flow_malloc(SIZEOF_FLOW_STRUCT);
-        src = ndpi_malloc(SIZEOF_ID_STRUCT);
-        dst = ndpi_malloc(SIZEOF_ID_STRUCT);
-    }
+  if (ndpi_info_mod == NULL) {
+    ndpi_info_mod = ndpi_init_detection_module(ndpi_no_prefs);
+    NDPI_PROTOCOL_BITMASK all;
+    NDPI_BITMASK_SET_ALL(all);
+    ndpi_set_protocol_detection_bitmask2(ndpi_info_mod, &all);
+    ndpi_flow = ndpi_flow_malloc(SIZEOF_FLOW_STRUCT);
+    src = ndpi_malloc(SIZEOF_ID_STRUCT);
+    dst = ndpi_malloc(SIZEOF_ID_STRUCT);
+  }
 
-    memset(ndpi_flow, 0, SIZEOF_FLOW_STRUCT);
-    memset(src, 0, SIZEOF_ID_STRUCT);
-    memset(dst, 0, SIZEOF_ID_STRUCT);
-    ndpi_detection_process_packet(ndpi_info_mod, ndpi_flow, Data, Size, 0, src, dst);
+  memset(ndpi_flow, 0, SIZEOF_FLOW_STRUCT);
+  memset(src, 0, SIZEOF_ID_STRUCT);
+  memset(dst, 0, SIZEOF_ID_STRUCT);
+  ndpi_detection_process_packet(ndpi_info_mod, ndpi_flow, Data, Size, 0, src, dst);
 
-    return 0;
+  return 0;
 }
