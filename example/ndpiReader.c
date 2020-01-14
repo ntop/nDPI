e1047b324bc67bd64f690b0848ad26464b6a982e
Alfredo Cardigliano <alfredo.cardigliano@gmail.com>
2020-01-14 10:57:05 +0100

    Fix misleading error message

--- example/ndpiReader.c
+++ example/ndpiReader.c
@@ -2696,6 +2696,7 @@ static pcap_t * openPcapFileOrDevice(u_int16_t thread_id, const u_char * pcap_fi
   if(dpdk_port_init(dpdk_port_id, mbuf_pool) != 0)
     rte_exit(EXIT_FAILURE, "DPDK: Cannot init port %u: please see README.dpdk\n", dpdk_port_id);
 #else
+  /* Trying to open the interface */
   if((pcap_handle = pcap_open_live((char*)pcap_file, snaplen,
 				   promisc, 500, pcap_error_buffer)) == NULL) {
     capture_for = capture_until = 0;
@@ -2703,15 +2704,18 @@ static pcap_t * openPcapFileOrDevice(u_int16_t thread_id, const u_char * pcap_fi
     live_capture = 0;
     num_threads = 1; /* Open pcap files in single threads mode */
 
-    /* trying to open a pcap file */
+    /* Trying to open a pcap file */
     if((pcap_handle = pcap_open_offline((char*)pcap_file, pcap_error_buffer)) == NULL) {
       char filename[256] = { 0 };
 
       if(strstr((char*)pcap_file, (char*)".pcap"))
 	printf("ERROR: could not open pcap file %s: %s\n", pcap_file, pcap_error_buffer);
+
+      /* Trying to open as a playlist as last attempt */
       else if((getNextPcapFileFromPlaylist(thread_id, filename, sizeof(filename)) != 0)
 	      || ((pcap_handle = pcap_open_offline(filename, pcap_error_buffer)) == NULL)) {
-        printf("ERROR: could not open playlist %s: %s\n", filename, pcap_error_buffer);
+        /* This probably was a bad interface name, printing a generic error */
+        printf("ERROR: could not open %s: %s\n", filename, pcap_error_buffer);
         exit(-1);
       } else {
         if((!quiet_mode))
