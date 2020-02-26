6fe95358812323fcf614fba8f8e5a43f43d164bb
Philippe Antoine <contact@catenacyber.fr>
2020-02-26 17:12:46 +0100

    Adds check before memcpy for bittorrent

--- src/lib/protocols/bittorrent.c
+++ src/lib/protocols/bittorrent.c
@@ -72,7 +72,7 @@ static void ndpi_add_connection_as_bittorrent(struct ndpi_detection_module_struc
     } else
       bt_hash = (const char*)&flow->packet.payload[28];
 
-    if(bt_hash) memcpy(flow->protos.bittorrent.hash, bt_hash, 20);
+    if(bt_hash && flow->packet.payload_packet_len >= 20 + (bt_hash-flow->packet.payload)) memcpy(flow->protos.bittorrent.hash, bt_hash, 20);
   }
 
   ndpi_int_change_protocol(ndpi_struct, flow, NDPI_PROTOCOL_BITTORRENT, NDPI_PROTOCOL_UNKNOWN);
