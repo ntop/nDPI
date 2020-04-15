9483c842b572a887c59d715d21b4737475ffd092
Philippe Antoine <contact@catenacyber.fr>
2020-04-15 16:24:03 +0200

    TLS initializes version_str

--- src/lib/protocols/tls.c
+++ src/lib/protocols/tls.c
@@ -1105,7 +1105,7 @@ int processClientServerHello(struct ndpi_detection_module_struct *ndpi_struct,
 		u_int8_t version_len = packet->payload[s_offset];
 		char version_str[256];
 		u_int8_t version_str_len = 0;
-		
+		version_str[0] = 0;
 #ifdef DEBUG_TLS
 		printf("Client SSL [TLS version len: %u]\n", version_len);
 #endif
