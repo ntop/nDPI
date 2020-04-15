287015ffdc33ff2c212563ffb866b38dee64e12b
Luca Deri <deri@ntop.org>
2020-04-15 22:42:09 +0200

    Fixed invalid check

--- src/lib/protocols/netbios.c
+++ src/lib/protocols/netbios.c
@@ -79,10 +79,10 @@ static void ndpi_int_netbios_add_connection(struct ndpi_detection_module_struct 
 					    u_int16_t sub_protocol) { 
   char name[64];
   u_int off = flow->packet.payload[12] == 0x20 ? 12 : 14;
-  
-  if(off > flow->packet.payload_packet_len &&
+
+  if((off < flow->packet.payload_packet_len) &&
      ndpi_netbios_name_interpret((char*)&flow->packet.payload[off], flow->packet.payload_packet_len - off, name, sizeof(name)) > 0)
-    snprintf((char*)flow->host_server_name, sizeof(flow->host_server_name)-1, "%s", name);    
+      snprintf((char*)flow->host_server_name, sizeof(flow->host_server_name)-1, "%s", name);    
 
   if(sub_protocol == NDPI_PROTOCOL_UNKNOWN)
     ndpi_set_detected_protocol(ndpi_struct, flow, NDPI_PROTOCOL_NETBIOS, NDPI_PROTOCOL_UNKNOWN);
