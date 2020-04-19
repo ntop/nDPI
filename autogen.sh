493bffb3e0bd616c2a333d563bc1dd4addec5359
Luca Deri <deri@ntop.org>
2020-04-19 21:48:24 +0200

    Added check for pkg-config presence

--- autogen.sh
+++ autogen.sh
@@ -12,6 +12,7 @@ AUTOMAKE=$(command -v automake)
 LIBTOOL=$(command -v libtool)
 LIBTOOLIZE=$(command -v libtoolize)
 AUTORECONF=$(command -v autoreconf)
+PKG_CONFIG=$(command -v pkg-config)
 
 if test -z $AUTOCONF; then
     echo "autoconf is missing: please install it and try again"
@@ -30,6 +31,11 @@ fi
 
 if test -z $AUTORECONF; then
     echo "autoreconf is missing: please install it and try again"
+    exit
+fi
+
+if test -z $PKG_CONFIG; then
+    echo "pkg-config is missing: please install it (apt-get install pkg-config) and try again"
     exit
 fi
 
