3e6bc6bf95cb6117b2195a624414bf6322ea3678
Didier <didgaudin@proginov.com>
2020-04-28 14:38:54 +0200

    second commit for delete duplicate string in flow->url

--- src/lib/protocols/http.c
+++ src/lib/protocols/http.c
@@ -208,12 +208,21 @@ static void check_content_type_and_change_protocol(struct ndpi_detection_module_
        && (packet->http_url_name.len > 0)
        && (packet->host_line.len > 0)) {
 	if( (packet->http_url_name.len == packet->host_line.len) && (strncmp((char*)packet->host_line.ptr, (char*)packet->http_url_name.ptr, packet->http_url_name.len)==0) ) {	
-		flow->http.url = ndpi_malloc(packet->host_line.len+1);
+		flow->http.url = ndpi_malloc(packet->host_line.len);
 		if(flow->http.url) {
 			strncpy(flow->http.url, (char*)packet->host_line.ptr, packet->host_line.len);
-			flow->http.url[ packet->host_line.len] = '\0';
+			flow->http.url[ packet->host_line.len ] = '\0';
 		}
 	}
+	else if( (packet->host_line.len < packet->http_url_name.len)  && (strstr((char*)packet->host_line.ptr,(char*)packet->http_url_name.ptr) !=NULL) ){
+			int len= packet->http_url_name.len ;
+			flow->http.url = ndpi_malloc(len);
+			if(flow->http.url) {
+				strncpy(flow->http.url, (char*)packet->host_line.ptr, packet->host_line.len);
+				flow->http.url[ packet->http_url_name.len ] = '\0';
+		}
+	}
+				
 	else {
 		int len = packet->http_url_name.len + packet->host_line.len + 1;
 		flow->http.url = ndpi_malloc(len);
