f83d0b18c67ffc942e01a36e9ead2a792d460c97
Luca Deri <deri@ntop.org>
2020-07-22 11:40:57 +0200

    Introduced SSH rick checks

--- src/lib/ndpi_utils.c
+++ src/lib/ndpi_utils.c
@@ -1524,6 +1524,12 @@ const char* ndpi_risk2str(ndpi_risk_enum risk) {
   case NDPI_MALFORMED_PACKET:
     return("Malformed packet");
 
+  case NDPI_SSH_OBSOLETE_CLIENT_VERSION_OR_CIPHER:
+    return("SSH obsolete client version/cipher");
+
+  case NDPI_SSH_OBSOLETE_SERVER_VERSION_OR_CIPHER:
+    return("SSH obsolete server version/cipher");
+    
   default:
     snprintf(buf, sizeof(buf), "%d", (int)risk);
     return(buf);
