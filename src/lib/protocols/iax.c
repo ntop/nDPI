4b1cae2c2be5be76e848c9794b62e5095a6b38f0
Luca Deri <deri@ntop.org>
2020-07-27 06:52:35 +0200

    Boundary check

--- src/lib/protocols/iax.c
+++ src/lib/protocols/iax.c
@@ -65,16 +65,17 @@ static void ndpi_search_setup_iax(struct ndpi_detection_module_struct *ndpi_stru
       ndpi_int_iax_add_connection(ndpi_struct, flow);
       return;
     }
+
     packet_len = 12;
-    for (i = 0; i < NDPI_IAX_MAX_INFORMATION_ELEMENTS; i++) {
+    for(i = 0; i < NDPI_IAX_MAX_INFORMATION_ELEMENTS; i++) {
+      if (packet_len >= packet->payload_packet_len)
+	break;      
+
       packet_len = packet_len + 2 + packet->payload[packet_len + 1];
-      if (packet_len == packet->payload_packet_len) {
+      if(packet_len == packet->payload_packet_len) {
 	NDPI_LOG_INFO(ndpi_struct, "found IAX\n");
 	ndpi_int_iax_add_connection(ndpi_struct, flow);
 	return;
-      }
-      if (packet_len > packet->payload_packet_len) {
-	break;
       }
     }
 
