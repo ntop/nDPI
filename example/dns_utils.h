afcb4e6fce9ee526365d6ed47e13451c4057848a
Roberto AGOSTINO <roberto.agostino@sistemiditlc.it>
2020-11-13 14:15:32 +0100

    fixing missing declaration

--- example/dns_utils.h
+++ example/dns_utils.h
@@ -10,6 +10,9 @@ extern char* dnsClass(char *ret, size_t len, enum DnsClass classIndex);
 extern char* dnsType(char *ret, size_t len, enum DnsType typeCode);
 extern char *dnsRData(char *ret, size_t len, struct dnsRR_t *rr );
 
+/* from dns.c */
+extern void clear_all_dns_list(struct ndpi_flow_struct *flow);
+
 extern void ndpi_patchIPv6Address(char *str);
 
 #endif
