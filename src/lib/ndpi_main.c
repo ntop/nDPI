e1be363037312cc107172a530a5c169e0105ba1a
Luca Deri <deri@ntop.org>
2020-12-23 20:38:02 +0000

    Removed space from protocol name

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -1452,7 +1452,7 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
 			  ndpi_build_default_ports(ports_a, 8009, 0, 0, 0, 0) /* TCP */,
 			  ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
   ndpi_set_proto_defaults(ndpi_str, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_TARGUS_GETDATA,
-			  0 /* can_have_a_subprotocol */, no_master, no_master, "Targus Dataspeed",
+			  0 /* can_have_a_subprotocol */, no_master, no_master, "TargusDataspeed",
 			  NDPI_PROTOCOL_CATEGORY_NETWORK,
 			  ndpi_build_default_ports(ports_a, 5001, 5201, 0, 0, 0) /* TCP */,
 			  ndpi_build_default_ports(ports_b, 5001, 5201, 0, 0, 0) /* UDP */);
