c65c0479ec782cc8e34c52a3673db7fbd14e5076
Toni Uhlig <matzeton@googlemail.com>
2021-02-03 12:46:36 +0100

    Added fuzzy targets conditional in tests/do.sh.in which prevents the fuzzer from running if nDPI was configured previously --enable-fuzztargets but not for the current config (may produce invalid results).
     * fixed possible NULL pointer dereference for memcpy(), src pointer should never be NULL
    
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -136,8 +136,11 @@ void *ndpi_realloc(void *ptr, size_t old_size, size_t new_size) {
   if(!ret)
     return(ret);
   else {
-    memcpy(ret, ptr, old_size);
-    ndpi_free(ptr);
+    if (ptr != NULL)
+    {
+      memcpy(ret, ptr, old_size);
+      ndpi_free(ptr);
+    }
     return(ret);
   }
 }
