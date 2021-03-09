477e4db650a4a565bb2288621adc01bb2a2dfed4
Luca Deri <deri@ntop.org>
2021-03-09 11:38:31 +0100

    Improved detection of Ookla speedtest and openspeedtest.com

--- src/lib/protocols/http.c
+++ src/lib/protocols/http.c
@@ -348,10 +348,16 @@ static void ndpi_http_parse_subprotocol(struct ndpi_detection_module_struct *ndp
     char *double_col = strchr((char*)flow->host_server_name, ':');
 
     if(double_col) double_col[0] = '\0';
-
-    ndpi_match_hostname_protocol(ndpi_struct, flow, NDPI_PROTOCOL_HTTP,
-				 (char *)flow->host_server_name,
-				 strlen((const char *)flow->host_server_name));
+    
+    if(ndpi_match_hostname_protocol(ndpi_struct, flow, NDPI_PROTOCOL_HTTP,
+				    (char *)flow->host_server_name,
+				    strlen((const char *)flow->host_server_name)) == 0) {
+      if((strstr(flow->http.url, ":8080/downloading?n=0.") == 0)
+	 || (strstr(flow->http.url, ":8080/upload?n=0.") == 0)) {
+	/* This looks like Ookla speedtest */
+	ndpi_set_detected_protocol(ndpi_struct, flow, NDPI_PROTOCOL_OOKLA, NDPI_PROTOCOL_HTTP);
+      }
+    }
   }
 }
 
