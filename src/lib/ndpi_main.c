0ef79983488b841c6144314b125eadc72b4178a8
Luca Deri <deri@ntop.org>
2021-04-25 10:06:22 +0200

    Removed protocol space in Genshin Impact

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -1625,7 +1625,7 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
 			  ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
 			  ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
   ndpi_set_proto_defaults(ndpi_str, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_GENSHIN_IMPACT,
-			  "Genshin Impact", NDPI_PROTOCOL_CATEGORY_GAME,
+			  "GenshinImpact", NDPI_PROTOCOL_CATEGORY_GAME,
 			  ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
 			  ndpi_build_default_ports(ports_b, 22102, 0, 0, 0, 0) /* UDP */);
 
