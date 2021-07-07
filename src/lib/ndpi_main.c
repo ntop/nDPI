5af940e33fbb6839ee9846a183e04cabdb70c60f
Vitaly Lavrov <vel21ripn@gmail.com>
2021-07-07 13:13:09 +0300

    Revert "Fix return value of ndpi_match_string_subprotocol() (#1230)"
    This reverts commit 58665e93a98d014b53d131b2481ccab074efc9ff.

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -6682,7 +6682,7 @@ int ndpi_match_string_subprotocol(struct ndpi_detection_module_struct *ndpi_str,
   rc = ndpi_match_string_common(((AC_AUTOMATA_t *) automa->ac_automa),
 		  string_to_match,string_to_match_len, &ret_match->protocol_id,
 		  &ret_match->protocol_category, &ret_match->protocol_breed);
-  return rc < 0 ? NDPI_PROTOCOL_UNKNOWN : ret_match->protocol_id;
+  return rc < 0 ? rc : ret_match->protocol_id;
 }
 
 /* **************************************** */
