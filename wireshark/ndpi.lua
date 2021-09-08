304a5075f44341d8ca515671c115df0a5e3781e4
deboracerretini <90244894+deboracerretini@users.noreply.github.com>
2021-09-08 19:00:51 +0200

    Add files via upload

--- wireshark/ndpi.lua
+++ wireshark/ndpi.lua
@@ -75,7 +75,7 @@ flow_risks[33] = ProtoField.bool("ndpi.flow_risk.suspicious_extension", "TLS sus
 flow_risks[34] = ProtoField.bool("ndpi.flow_risk.fatal_alert", "TLS fatal alert detected", num_bits_flow_risks, nil, bit(2), "nDPI Flow Risk: TLS fatal alert")
 flow_risks[35] = ProtoField.bool("ndpi.flow_risk.suspicious_entropy", "Suspicious entropy", num_bits_flow_risks, nil, bit(3), "nDPI Flow Risk: suspicious entropy")
 
-flow_risks[36] = ProtoField.bool("ndpi.flow_risk.dns_dimension_alert", "DNS dimension alert", num_bits_flow_risks, nil, bit(0), "nDPI Flow Risk: DNS packet is larger than 512 bytes")
+flow_risks[36] = ProtoField.bool("ndpi.flow_risk.dns_large_packet", "DNS large packet", num_bits_flow_risks, nil, bit(0), "nDPI Flow Risk: DNS packet is larger than 512 bytes")
 flow_risks[37] = ProtoField.bool("ndpi.flow_risk.dns_fragmented", "DNS fragmented", num_bits_flow_risks, nil, bit(0), "nDPI Flow Risk: DNS message is fragmented")
 
 -- Last one: keep in sync the bitmask when adding new risks!!
