ead216b9518f06dbeb452883c77a9599d5f8689f
Nardi Ivan <nardi.ivan@gmail.com>
2022-01-16 19:18:31 +0100

    Zattoo: fix Null-dereference READ with ipv6 traffic
    Fix: 20b5f6d7
    Detected by oss-fux:
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=43700

--- src/lib/protocols/zattoo.c
+++ src/lib/protocols/zattoo.c
@@ -100,7 +100,7 @@ void ndpi_search_zattoo(struct ndpi_detection_module_struct *ndpi_struct, struct
       ndpi_parse_packet_line_info(ndpi_struct, flow);
 
       // test for unique character of the zattoo header
-      if(packet->parsed_lines == 4 && packet->host_line.ptr != NULL) {
+      if(packet->parsed_lines == 4 && packet->host_line.ptr != NULL && packet->iph) {
 	u_int32_t ip;
 	u_int16_t bytes_read = 0;
 
