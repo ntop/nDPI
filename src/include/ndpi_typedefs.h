dfbe863f4e91090c35f69a334e7f74ee8e761143
Luca Deri <deri@ntop.org>
2022-05-29 16:35:08 +0200

    Comment

--- src/include/ndpi_typedefs.h
+++ src/include/ndpi_typedefs.h
@@ -111,7 +111,7 @@ typedef enum {
   NDPI_DNS_LARGE_PACKET,
   NDPI_DNS_FRAGMENTED,
   NDPI_INVALID_CHARACTERS,
-  NDPI_POSSIBLE_EXPLOIT, /* Log4J and other exploits */
+  NDPI_POSSIBLE_EXPLOIT, /* Log4J, Wordpress and other exploits */
   NDPI_TLS_CERTIFICATE_ABOUT_TO_EXPIRE,
   NDPI_PUNYCODE_IDN, /* https://en.wikipedia.org/wiki/Punycode */
   NDPI_ERROR_CODE_DETECTED,
