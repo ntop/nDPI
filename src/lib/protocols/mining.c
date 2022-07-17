c9840ec36549b6e65ce1da89e7fd1109e42dd807
Nardi Ivan <nardi.ivan@gmail.com>
2022-07-17 12:49:18 +0200

    DTLS: fix exclusion of DTLS protocol
    Add an helper to exclude a generic protocol

--- src/lib/protocols/mining.c
+++ src/lib/protocols/mining.c
@@ -67,7 +67,7 @@ static void ndpi_search_mining_udp(struct ndpi_detection_module_struct *ndpi_str
     }
   }
   
-  ndpi_exclude_protocol(ndpi_struct, flow, NDPI_PROTOCOL_MINING, __FILE__, __FUNCTION__, __LINE__);  
+  NDPI_EXCLUDE_PROTO(ndpi_struct, flow);
 }
 
 /* ************************************************************************** */
@@ -159,7 +159,7 @@ static void ndpi_search_mining_tcp(struct ndpi_detection_module_struct *ndpi_str
     }
   }
 
-  ndpi_exclude_protocol(ndpi_struct, flow, NDPI_PROTOCOL_MINING, __FILE__, __FUNCTION__, __LINE__);
+  NDPI_EXCLUDE_PROTO(ndpi_struct, flow);
 }
 
 /* ************************************************************************** */
