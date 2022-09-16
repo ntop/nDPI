ec3e2db00daaf2dc051a95b986cbbac780bced40
Toni Uhlig <matzeton@googlemail.com>
2022-09-16 11:51:08 +0200

    Add Syncthing dissector.
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -1982,6 +1982,10 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
                           "NAT-PMP", NDPI_PROTOCOL_CATEGORY_NETWORK,
                           ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
                           ndpi_build_default_ports(ports_b, 5351, 0, 0, 0, 0) /* UDP */);
+  ndpi_set_proto_defaults(ndpi_str, 0 /* encrypted */, 0 /* nw proto */, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_SYNCTHING,
+                          "Syncthing", NDPI_PROTOCOL_CATEGORY_DOWNLOAD_FT,
+                          ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
+                          ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_main.c"
@@ -4583,6 +4587,9 @@ static int ndpi_callback_init(struct ndpi_detection_module_struct *ndpi_str) {
 
   /* NATPMP */
   init_natpmp_dissector(ndpi_str, &a, detection_bitmask);
+
+  /* Syncthing */
+  init_syncthing_dissector(ndpi_str, &a, detection_bitmask);
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_main_init.c"
