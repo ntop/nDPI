6a863befe4597b42e80b7e946a2fcde3093d2f4c
Mahmoud Maatuq <mahmoudmatook.mm@gmail.com>
2023-03-15 23:11:28 +0400

    remove redifinition of vxlanhdr struct and use existing struct ndpi_vxlanhdr in src/include/ndpi_typedefs.h

--- example/reader_util.c
+++ example/reader_util.c
@@ -1745,22 +1745,19 @@ int ndpi_is_datalink_supported(int datalink_type) {
   }
 }
 
-struct vxlan_packet_header {
-  u_int8_t flags[4]; /* the first byte is flags, other three are reserved */
-  u_int8_t vni[4];   /* the first three bytes are VNI, the last byte is reserved */
-};
 static bool ndpi_is_valid_vxlan(const struct pcap_pkthdr *header, const u_char *packet, u_int16_t ip_offset, u_int16_t ip_len){
-  if(header->caplen >= ip_offset + ip_len + sizeof(struct vxlan_packet_header)) {
+  if(header->caplen >= ip_offset + ip_len + sizeof(struct ndpi_vxlanhdr)) {
     u_int32_t vxlan_dst_port  = ntohs(4789);
     u_int32_t expected_flags = 0x08; /* only one bit should be set in the first byte */
     struct ndpi_udphdr *udp = (struct ndpi_udphdr *)&packet[ip_offset+ip_len];
     u_int offset = ip_offset + ip_len + sizeof(struct ndpi_udphdr);
-    struct vxlan_packet_header *vxlan = (struct vxlan_packet_header *)&packet[offset];
+    struct ndpi_vxlanhdr *vxlan = (struct ndpi_vxlanhdr *)&packet[offset];
 
     if((udp->dest == vxlan_dst_port || udp->source == vxlan_dst_port) &&
-      (vxlan->flags[0] == expected_flags) && (vxlan->flags[1] == 0x0) &&
-      (vxlan->flags[2] == 0x0) && (vxlan->flags[3] == 0x0) &&
-      (vxlan->vni[3] == 0x0)) {
+      ((vxlan->flags & 0xFF) == expected_flags) &&
+      (((vxlan->flags >> 8) & 0xFF) == 0x0) &&
+      ((vxlan->groupPolicy & 0xFFFF) == 0x0) &&
+      (((vxlan->vni >> 24) & 0xFF) == 0x0)) {
       return true;
     }
   }
@@ -1768,7 +1765,7 @@ static bool ndpi_is_valid_vxlan(const struct pcap_pkthdr *header, const u_char *
 }
 
 static inline u_int ndpi_skip_vxlan(u_int16_t ip_offset, u_int16_t ip_len){
-  return ip_offset + ip_len + sizeof(struct ndpi_udphdr) + sizeof(struct vxlan_packet_header);
+  return ip_offset + ip_len + sizeof(struct ndpi_udphdr) + sizeof(struct ndpi_vxlanhdr);
 }
 
 /* ****************************************************** */
