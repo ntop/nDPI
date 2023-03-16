994eb23c332feaf153176a0df213a9870568f372
Mahmoud Maatuq <mahmoudmatook.mm@gmail.com>
2023-03-17 01:17:02 +0400

    fix endianess issue by directly accessing bytes using packet index
    * tested for big endian using docker image multiarch/ubuntu-core:s390x-bionic

--- example/reader_util.c
+++ example/reader_util.c
@@ -1746,21 +1746,44 @@ int ndpi_is_datalink_supported(int datalink_type) {
 }
 
 static bool ndpi_is_valid_vxlan(const struct pcap_pkthdr *header, const u_char *packet, u_int16_t ip_offset, u_int16_t ip_len){
-  if(header->caplen >= ip_offset + ip_len + sizeof(struct ndpi_vxlanhdr)) {
-    u_int32_t vxlan_dst_port  = ntohs(4789);
-    u_int32_t expected_flags = 0x08; /* only one bit should be set in the first byte */
-    struct ndpi_udphdr *udp = (struct ndpi_udphdr *)&packet[ip_offset+ip_len];
-    u_int offset = ip_offset + ip_len + sizeof(struct ndpi_udphdr);
-    struct ndpi_vxlanhdr *vxlan = (struct ndpi_vxlanhdr *)&packet[offset];
-
-    if((udp->dest == vxlan_dst_port || udp->source == vxlan_dst_port) &&
-      ((vxlan->flags & 0xFF) == expected_flags) &&
-      (((vxlan->flags >> 8) & 0xFF) == 0x0) &&
-      ((vxlan->groupPolicy & 0xFFFF) == 0x0) &&
-      (((vxlan->vni >> 24) & 0xFF) == 0x0)) {
-      return true;
-    }
+  if(header->caplen < ip_offset + ip_len + sizeof(struct ndpi_vxlanhdr)) {
+    return false;
   }
+  u_int32_t vxlan_dst_port  = ntohs(4789);
+  struct ndpi_udphdr *udp = (struct ndpi_udphdr *)&packet[ip_offset+ip_len];
+  u_int offset = ip_offset + ip_len + sizeof(struct ndpi_udphdr);
+  /**
+   * rfc-7348 
+   *    VXLAN Header:  This is an 8-byte field that has:
+
+    - Flags (8 bits): where the I flag MUST be set to 1 for a valid
+      VXLAN Network ID (VNI).  The other 7 bits (designated "R") are
+      reserved fields and MUST be set to zero on transmission and
+      ignored on receipt.
+
+    - VXLAN Segment ID/VXLAN Network Identifier (VNI): this is a
+      24-bit value used to designate the individual VXLAN overlay
+      network on which the communicating VMs are situated.  VMs in
+      different VXLAN overlay networks cannot communicate with each
+      other.
+
+    - Reserved fields (24 bits and 8 bits): MUST be set to zero on
+      transmission and ignored on receipt.
+         VXLAN Header:
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |R|R|R|R|I|R|R|R|            Reserved                           |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |                VXLAN Network Identifier (VNI) |   Reserved    |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  */
+  if((udp->dest == vxlan_dst_port || udp->source == vxlan_dst_port) &&
+    (packet[offset] == 0x8) &&
+    (packet[offset + 1] == 0x0) &&
+    (packet[offset + 2] == 0x0) &&
+    (packet[offset + 3] == 0x0) &&
+    (packet[offset + 7] ==  0x0)) {
+    return true;
+    }
   return false;
 }
 
