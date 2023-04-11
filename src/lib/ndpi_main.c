6660feed3509db0eaa91d03dd5a1b0da1195abbd
Toni <matzeton@googlemail.com>
2023-04-11 20:26:27 +0200

    Added BACnet dissector. (#1940)
    Signed-off-by: lns <matzeton@googlemail.com>
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -2056,6 +2056,10 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
                           "Source_Engine", NDPI_PROTOCOL_CATEGORY_GAME,
                           ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
                           ndpi_build_default_ports(ports_b, 27015, 0, 0, 0, 0) /* UDP */);
+  ndpi_set_proto_defaults(ndpi_str, 1 /* cleartext */, 0 /* nw proto */, NDPI_PROTOCOL_SAFE, NDPI_PROTOCOL_BACNET,
+                          "BACnet", NDPI_PROTOCOL_CATEGORY_IOT_SCADA,
+                          ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
+                          ndpi_build_default_ports(ports_b, 47808, 0, 0, 0, 0) /* UDP */);
 
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
@@ -4891,6 +4895,9 @@ static int ndpi_callback_init(struct ndpi_detection_module_struct *ndpi_str) {
 
   /* Source Engine */
   init_source_engine_dissector(ndpi_str, &a);
+
+  /* BACnet */
+  init_bacnet_dissector(ndpi_str, &a);
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_main_init.c"
