b02fb7e7cb3530b2fc6a65d979567fff0716a7c0
nikitamishagin <nikitamishagin@gmail.com>
2023-04-20 17:20:36 +0400

    Remove spaces in "hots" protocol name

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -2068,7 +2068,7 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
                           ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
                           ndpi_build_default_ports(ports_b, 8000, 0, 0, 0, 0) /* UDP */);
   ndpi_set_proto_defaults(ndpi_str, 1 /* cleartext */, 0 /* nw proto */, NDPI_PROTOCOL_FUN, NDPI_PROTOCOL_HOTS,
-                          "Heroes of the Storm", NDPI_PROTOCOL_CATEGORY_GAME,
+                          "Heroes_of_the_Storm", NDPI_PROTOCOL_CATEGORY_GAME,
                           ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0),  /* TCP */
                           ndpi_build_default_ports(ports_b, 3724, 0, 0, 0, 0)); /* UDP */
 
