0c028d5b9c384c0d29cc40f1b237eb081a8903fa
nikitamishagin <nikitamishagin@gmail.com>
2023-04-21 12:15:06 +0400

    Fix hots default ports

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -2062,8 +2062,8 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
                           ndpi_build_default_ports(ports_b, 47808, 0, 0, 0, 0) /* UDP */);
   ndpi_set_proto_defaults(ndpi_str, 1 /* cleartext */, 0 /* nw proto */, NDPI_PROTOCOL_FUN, NDPI_PROTOCOL_HOTS,
         "Heroes_of_the_Storm", NDPI_PROTOCOL_CATEGORY_GAME,
-        ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0),  /* TCP */
-        ndpi_build_default_ports(ports_b, 3724, 0, 0, 0, 0)); /* UDP */
+        ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
+        ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
 
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
