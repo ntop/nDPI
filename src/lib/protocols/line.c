46aab35a296bfe059e60ea7aa3dc75ca6c0b88a8
Ivan Nardi <12729895+IvanNardi@users.noreply.github.com>
2023-06-21 09:22:07 +0200

    Line: fix heap-buffer-overflow error (#2015)

--- src/lib/protocols/line.c
+++ src/lib/protocols/line.c
@@ -63,7 +63,7 @@ static void ndpi_search_line(struct ndpi_detection_module_struct *ndpi_struct,
     if ((packet->payload_packet_len == 46 && ntohl(get_u_int32_t(packet->payload, 0)) == 0xb6130006) ||
 	(packet->payload_packet_len == 8 && ntohl(get_u_int32_t(packet->payload, 0)) == 0xb6070004) ||
 	(packet->payload_packet_len == 16 && ntohl(get_u_int32_t(packet->payload, 0)) == 0xb609000c) ||
-	(ndpi_struct->packet.payload[0] == 0xD0 &&
+	(packet->payload_packet_len >= 2 /* TODO */ && ndpi_struct->packet.payload[0] == 0xD0 &&
 	 (ndpi_struct->packet.payload[1] == 0xB3 || ndpi_struct->packet.payload[1] == 0xB4
 	  || ndpi_struct->packet.payload[1] == 0xDA || ndpi_struct->packet.payload[1] == 0xDB))) {
       ndpi_int_line_add_connection(ndpi_struct, flow);
