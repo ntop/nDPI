95c8717bddfa8b6208420672cc3d43664de1eb31
Ivan Kapranov <i.kapranov@securitycode.ru>
2023-10-31 15:09:09 +0300

    Fix name missmatch for Sina and Sina Weibo

--- src/include/ndpi_protocol_ids.h
+++ src/include/ndpi_protocol_ids.h
@@ -384,6 +384,7 @@ typedef enum {
   NDPI_PROTOCOL_PROTOBUF              = 353,
   NDPI_PROTOCOL_ETHEREUM              = 354, 
   NDPI_PROTOCOL_TELEGRAM_VOIP         = 355,
+  NDPI_PROTOCOL_SINA_WEIBO	      = 356,	
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_protocol_ids.h"
