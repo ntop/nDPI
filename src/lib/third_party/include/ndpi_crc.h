9ab52a028d4fbf3706607b1ff6e77d217dc28567
0xA50C1A1 <mage.wizard.88@gmail.com>
2023-12-05 15:16:12 +0300

    Move crc16 stuff to ndpi_analyze.c

--- src/lib/third_party/include/ndpi_crc.h
+++
@@ -1,8 +0,0 @@
-#ifndef _NDPI_CRC_
-#define _NDPI_CRC_
-
-u_int16_t ndpi_crc16_ccit(const void* data, size_t n_bytes);
-u_int16_t ndpi_crc16_ccit_false(const void *data, size_t n_bytes);
-u_int16_t ndpi_crc16_xmodem(const void *data, size_t n_bytes);
-
-#endif
