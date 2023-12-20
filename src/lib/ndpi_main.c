a8a85ba6ee60812e5b35585e062e8ea8c41b7d43
0xA50C1A1 <mage.wizard.88@gmail.com>
2023-12-20 05:41:41 +0300

    Add OpenFlow protocol dissector

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -2198,6 +2198,10 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
         "UFTP", NDPI_PROTOCOL_CATEGORY_DOWNLOAD_FT,
         ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
         ndpi_build_default_ports(ports_b, 1044, 0, 0, 0, 0) /* UDP */);
+  ndpi_set_proto_defaults(ndpi_str, 1 /* cleartext */, 0 /* nw proto */, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_OPENFLOW,
+			  "OpenFlow", NDPI_PROTOCOL_CATEGORY_NETWORK,
+			  ndpi_build_default_ports(ports_a, 6653, 0, 0, 0, 0) /* TCP */,
+			  ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_main.c"
@@ -5694,6 +5698,9 @@ static int ndpi_callback_init(struct ndpi_detection_module_struct *ndpi_str) {
 
   /* UFTP */
   init_uftp_dissector(ndpi_str, &a);
+
+  /* OpenFlow */
+  init_openflow_dissector(ndpi_str, &a);
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_main_init.c"
