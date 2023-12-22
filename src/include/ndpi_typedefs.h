a95d4744b61a0c32a08603c11e256bac1ac42ba7
Luca Deri <deri@ntop.org>
2023-12-22 20:52:56 +0100

    Fixes nDPI datastructs size check

--- src/include/ndpi_typedefs.h
+++ src/include/ndpi_typedefs.h
@@ -1476,11 +1476,11 @@ struct ndpi_flow_struct {
 
 #if !defined(NDPI_CFFI_PREPROCESSING) && defined(__linux__)
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
-_Static_assert(sizeof(((struct ndpi_flow_struct *)0)->protos) <= 216,
-               "Size of the struct member protocols increased to more than 216 bytes, "
+_Static_assert(sizeof(((struct ndpi_flow_struct *)0)->protos) <= 224,
+               "Size of the struct member protocols increased to more than 224 bytes, "
                "please check if this change is necessary.");
-_Static_assert(sizeof(struct ndpi_flow_struct) <= 968,
-               "Size of the flow struct increased to more than 968 bytes, "
+_Static_assert(sizeof(struct ndpi_flow_struct) <= 976,
+               "Size of the flow struct increased to more than 976 bytes, "
                "please check if this change is necessary.");
 #endif
 #endif
