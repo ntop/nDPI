45de179f18c735c6976a84243b724a9ae3a05ba7
Nardi Ivan <nardi.ivan@gmail.com>
2024-02-19 21:01:31 +0100

    Add identification of Huawei generic and cloud traffic

--- src/include/ndpi_protocol_ids.h
+++ src/include/ndpi_protocol_ids.h
@@ -426,6 +426,8 @@ typedef enum {
   NDPI_PROTOCOL_TENCENTGAMES          = 395,
   NDPI_PROTOCOL_GAIJIN                = 396,
   NDPI_PROTOCOL_C1222                 = 397,
+  NDPI_PROTOCOL_HUAWEI                = 398,
+  NDPI_PROTOCOL_HUAWEI_CLOUD          = 399,
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_protocol_ids.h"
