12e142565ea9b42260808a5269c3cf8bf1978721
Ivan Nardi <12729895+IvanNardi@users.noreply.github.com>
2024-02-20 11:51:58 +0100

    Add a script to download/update the domain suffix list (#2321)

---
+++ utils/public_suffix_list_download.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "${0}")" || exit 1
+. ./common.sh || exit 1
+
+LIST=../lists/public_suffix_list.dat
+
+printf '(1) %s\n' "Getting domain suffix list"
+DOMAINS="$(curl -s 'https://publicsuffix.org/list/public_suffix_list.dat')"
+is_str_empty "${DOMAINS}" "Please check the URL."
+
+echo "${DOMAINS}" > ${LIST}
+
+printf '(3) %s\n' "List ${LIST} is now ready"
+exit 0
