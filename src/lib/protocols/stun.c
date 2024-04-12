df99a53d34f07cfa4d177c8047c12dc6cfee2d87
Toni Uhlig <matzeton@googlemail.com>
2024-04-12 18:57:30 +0200

    fix other STUN invalid read(s)
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- src/lib/protocols/stun.c
+++ src/lib/protocols/stun.c
@@ -335,7 +335,7 @@ int is_stun(struct ndpi_detection_module_struct *ndpi_struct,
     switch(attribute) {
     case 0x0001: /* MAPPED-ADDRESS */
       if(ndpi_struct->cfg.stun_mapped_address_enabled &&
-	 real_len < payload_length - off - 5) {
+	 real_len < payload_length - off - 9) {
 	u_int8_t protocol_family = payload[off+5];
 
 	if(protocol_family == 0x01 /* IPv4 */) {
@@ -484,7 +484,7 @@ int is_stun(struct ndpi_detection_module_struct *ndpi_struct,
 
     case 0x0020: /* XOR-MAPPED-ADDRESS */
       if(ndpi_struct->cfg.stun_mapped_address_enabled &&
-	 real_len <= payload_length - off - 4) {
+	 real_len < payload_length - off - 5) {
 	u_int8_t protocol_family = payload[off+5];
 
 	if(protocol_family == 0x01 /* IPv4 */) {
