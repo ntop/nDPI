77b71d7dabeab1f4db6bb15303f602178b9bb2e8
0x41CEA55 <russlanddk@yandex.ru>
2024-04-19 17:06:54 +0300

    Replace my personal email with my corporate one in all my contributions

--- src/lib/protocols/c1222.c
+++ src/lib/protocols/c1222.c
@@ -4,7 +4,7 @@
  * ANSI C12.22 (IEEE Std 1703)
  * 
  * Copyright (C) 2024 - ntop.org
- * Copyright (C) 2024 - V.G <jacendi@protonmail.com>
+ * Copyright (C) 2024 - V.G <v.gavrilov@securitycode.ru>
  *
  * This file is part of nDPI, an open source deep packet inspection
  * library based on the OpenDPI and PACE technology by ipoque GmbH
