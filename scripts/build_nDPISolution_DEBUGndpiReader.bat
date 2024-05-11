990a990d42d2cec67ec2d97f42ffa51abf26fd51
akumar <akumar@trapptechnology.com>
2024-05-11 13:28:57 -0700

    Submit script file too

---
+++ scripts/build_nDPISolution_DEBUGndpiReader.bat
@@ -0,0 +1,2 @@
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"  "C:\Projects\nDPIFork2\windows\nDPI.sln" /property:Configuration=Debug-ndpiReader
