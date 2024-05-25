a8f9dd32d728845104f0fa94fb9de3880c87bfb0
Toni Uhlig <matzeton@googlemail.com>
2024-05-25 08:48:22 +0200

    Add ZUG consensus protocol dissector.
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -2270,6 +2270,10 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
 			  "CoD_Mobile", NDPI_PROTOCOL_CATEGORY_GAME,
  			  ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
 			  ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
+  ndpi_set_proto_defaults(ndpi_str, 0 /* encrypted */, 0 /* nw proto */, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_ZUG,
+			  "ZUG", NDPI_PROTOCOL_CATEGORY_CRYPTO_CURRENCY,
+			  ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
+			  ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_main.c"
@@ -6178,6 +6182,9 @@ static int ndpi_callback_init(struct ndpi_detection_module_struct *ndpi_str) {
 
   /* Call of Duty: Mobile */
   init_cod_mobile_dissector(ndpi_str, &a);
+
+  /* ZUG */
+  init_zug_dissector(ndpi_str, &a);
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_main_init.c"
