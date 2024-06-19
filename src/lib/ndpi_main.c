99f367b712227ea1ec1fe6ef971b0c6fbf28aebd
Mark Jeffery <dandare100@gmail.com>
2024-06-19 13:09:18 +0200

    Widened columns, static method and remove ndpi prefix

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -9548,7 +9548,7 @@ int ndpi_get_category_id(struct ndpi_detection_module_struct *ndpi_str, char *ca
 /* ****************************************************** */
 
 
-char *ndpi_default_ports_string(char *ports_str,u_int16_t *default_ports){
+static char *default_ports_string(char *ports_str,u_int16_t *default_ports){
 
     //dont display zero ports on help screen
     if (default_ports[0] == 0)
@@ -9585,15 +9585,15 @@ void ndpi_dump_protocols(struct ndpi_detection_module_struct *ndpi_str, FILE *du
     char udp_ports[30] = "";
     char tcp_ports[30] = "";
 
-    fprintf(dump_out, "%3d %8d %-22s %-10s %-8s %-12s %-18s %-20s %-20s\n",
+    fprintf(dump_out, "%3d %8d %-22s %-10s %-8s %-12s %-18s %-31s %-31s\n",
 	   i, ndpi_map_ndpi_id_to_user_proto_id(ndpi_str, i),
 	   ndpi_str->proto_defaults[i].protoName,
 	   ndpi_get_l4_proto_name(ndpi_get_l4_proto_info(ndpi_str, i)),
 	   ndpi_str->proto_defaults[i].isAppProtocol ? "" : "X",
 	   ndpi_get_proto_breed_name(ndpi_str->proto_defaults[i].protoBreed),
 	   ndpi_category_get_name(ndpi_str, ndpi_str->proto_defaults[i].protoCategory),
-     ndpi_default_ports_string(udp_ports,ndpi_str->proto_defaults[i].udp_default_ports),
-     ndpi_default_ports_string(tcp_ports,ndpi_str->proto_defaults[i].tcp_default_ports)
+     default_ports_string(udp_ports,ndpi_str->proto_defaults[i].udp_default_ports),
+     default_ports_string(tcp_ports,ndpi_str->proto_defaults[i].tcp_default_ports)
      );
 
   }
