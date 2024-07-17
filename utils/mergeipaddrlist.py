0a3a82680d6171a005fa944916ef551b4c3aafa7
Petr <30545094+pasabanov@users.noreply.github.com>
2024-07-17 12:00:42 +0300

    python: reformatted, fixed bugs (#2504)

--- utils/mergeipaddrlist.py
+++ utils/mergeipaddrlist.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 
 import sys
-import socket
-import struct
 import netaddr
 
-if len (sys.argv) == 3:
+if len(sys.argv) == 3:
     proto = sys.argv[2]
 
 if len(sys.argv) < 2:
     print("Usage: mergeipaddrlist.py <file>")
-    sys.exit (1)
+    sys.exit(1)
 
 ipFile = open(sys.argv[1])
 ipAddresses = list(ipFile.readlines())
-ipAddresses = [x.replace("\n","") for x in ipAddresses]
+ipAddresses = [x.replace("\n", "") for x in ipAddresses]
 ipAddresses = sorted(ipAddresses)
 cidrs = netaddr.cidr_merge(ipAddresses)
 
