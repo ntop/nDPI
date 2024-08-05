77a79c266d3ceb884acdeabd7dff77d0cbcf2a54
Pavel Vasilyev <pvasilev@securitycode.ru>
2024-08-06 01:08:13 +0300

    Revert "Fix Memory leak in ahocorasick #2258"
    This reverts commit e482e285f9ff593cfe97b138225d95708878acbf.

--- src/lib/third_party/src/ahocorasick.c
+++ src/lib/third_party/src/ahocorasick.c
@@ -226,7 +226,6 @@ AC_ERROR_t ac_automata_add (AC_AUTOMATA_t * thiz, AC_PATTERN_t * patt)
 {
   unsigned int i;
   AC_NODE_t * n;
-  AC_NODE_t * rev[patt->length];
   AC_NODE_t * next;
   AC_ALPHABET_t alpha;
 
@@ -251,14 +250,10 @@ AC_ERROR_t ac_automata_add (AC_AUTOMATA_t * thiz, AC_PATTERN_t * patt)
 
       if((next = node_find_next(n, alpha)) != 0) {
           n = next;
-          rev[i] = next;
           continue;
       }
-      if(!(next = node_create_next(n, alpha))) {
-            while (--i)
-                  node_release(rev[i], 1);
+      if(!(next = node_create_next(n, alpha))) 
               return ACERR_ERROR;
-      }
       next->id = ++thiz->id;
       thiz->all_nodes_num++;
       n = next;
@@ -279,18 +274,12 @@ AC_ERROR_t ac_automata_add (AC_AUTOMATA_t * thiz, AC_PATTERN_t * patt)
     /* original code */
     patt->rep.number = n->matched_patterns->patterns[0].rep.number;
 #endif
-    while (--i)
-          node_release(rev[i], 1);
-
     return ACERR_DUPLICATE_PATTERN;
   }
 
-  if(node_register_matchstr(n, patt, 0)) {
-      while (--i)
-           node_release(rev[i], 1);
+  if(node_register_matchstr(n, patt, 0))
       return ACERR_ERROR;
-  }
-
+ 
   thiz->total_patterns++;
 
   return ACERR_SUCCESS;
