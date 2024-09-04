eb71914784365984d7d4bc74165f7d0ecf7f346b
Nardi Ivan <nardi.ivan@gmail.com>
2024-09-04 22:11:18 +0200

    Add an heuristic to detect encrypted/obfuscated OpenVPN flows
    Based on the paper: "OpenVPN is Open to VPN Fingerprinting"
    See: https://www.usenix.org/conference/usenixsecurity22/presentation/xue-diwen
    
    Basic idea:
    * the distribution of the first byte of the messages (i.e. the distribution
    of the op-codes) is quite unique
    * this fingerprint might be still detectable even if the OpenVPN packets are
    somehow fully encrypted/obfuscated
    
    The heuristic is disabled by default.

--- fuzz/fuzz_config.cpp
+++ fuzz/fuzz_config.cpp
@@ -242,6 +242,14 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   if(fuzzed_data.ConsumeBool()) {
     value = fuzzed_data.ConsumeIntegralInRange(0, 0x01 + 1);
     snprintf(cfg_value, sizeof(cfg_value), "%d", value);
+    ndpi_set_config(ndpi_info_mod, "openvpn", "dpi.heuristics", cfg_value);
+    value = fuzzed_data.ConsumeIntegralInRange(0, 255 + 1);
+    snprintf(cfg_value, sizeof(cfg_value), "%d", value);
+    ndpi_set_config(ndpi_info_mod, "openvpn", "dpi.heuristics.num_messages", cfg_value);
+  }
+  if(fuzzed_data.ConsumeBool()) {
+    value = fuzzed_data.ConsumeIntegralInRange(0, 0x01 + 1);
+    snprintf(cfg_value, sizeof(cfg_value), "%d", value);
     ndpi_set_config(ndpi_info_mod, "openvpn", "subclassification_by_ip", cfg_value);
   }
   if(fuzzed_data.ConsumeBool()) {
