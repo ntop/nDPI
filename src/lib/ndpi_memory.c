7fcf54dd8489f77352798e23d0067fe256409b75
Toni <matzeton@googlemail.com>
2024-10-21 16:17:23 +0200

    Fix `ndpi_tot_allocated_memory` calculation if `ndpi_calloc()` used (#2604)
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- src/lib/ndpi_memory.c
+++ src/lib/ndpi_memory.c
@@ -64,11 +64,11 @@ void *ndpi_malloc(size_t size) {
 
 void *ndpi_calloc(unsigned long count, size_t size) {
   size_t len = count * size;
-  void *p = ndpi_malloc(len);
+  void *p = _ndpi_malloc ? _ndpi_malloc(len) : malloc(len);
 
   if(p) {
     memset(p, 0, len);
-    __sync_fetch_and_add(&ndpi_tot_allocated_memory, size);
+    __sync_fetch_and_add(&ndpi_tot_allocated_memory, len);
   }
 
   return(p);
