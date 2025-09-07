05b982bf6f75cd6d67e8c1a359a3f751af2b8f5e
Toni Uhlig <matzeton@googlemail.com>
2025-09-07 18:22:38 +0200

    Fix roaring_v2 build issue if compiling with `-Werrror -Wall -Wextra -fno-inline`
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- src/lib/third_party/src/roaring_v2.c
+++ src/lib/third_party/src/roaring_v2.c
@@ -4437,9 +4437,11 @@ static inline container_t *get_writable_copy_if_shared(
  * End of shared container code
  */
 
+#ifdef NDPI_ENABLE_DEBUG_MESSAGES
 static const char *container_names[] = {"bitset", "array", "run", "shared"};
 static const char *shared_container_names[] = {
     "bitset (shared)", "array (shared)", "run (shared)"};
+#endif
 
 // no matter what the initial container was, convert it to a bitset
 // if a new container is produced, caller responsible for freeing the previous
@@ -4487,6 +4489,7 @@ static inline bitset_container_t *container_to_bitset(
     }
 }*/
 
+#ifdef NDPI_ENABLE_DEBUG_MESSAGES
 static inline const char *get_full_container_name(
     const container_t *c, uint8_t typecode
 ){
@@ -4519,6 +4522,7 @@ static inline const char *get_full_container_name(
     __builtin_unreachable();
     return NULL;
 }
+#endif
 
 /**
  * Get the container cardinality (number of elements), requires a  typecode
