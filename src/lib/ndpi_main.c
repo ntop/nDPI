ffa51ac1d808343deeb29a0d95b62c538d5592e5
Ivan Nardi <nardi.ivan@gmail.com>
2025-10-20 14:07:43 +0200

    Allow to overwrite domain matching via custom rules
    This is basically the revert of 0db12b1390 and 43d9caac00.
    Add some tests about this feature

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -4233,13 +4233,6 @@ struct ndpi_detection_module_struct *ndpi_init_detection_module(struct ndpi_glob
     return(NULL);
   }
 
-  /*
-    Load defaults first so that they can be overwritten
-    for instance using protos.txt via ndpi_load_protocols_file()
-
-   */
-  load_string_based_protocols(ndpi_str);
-
   return(ndpi_str);
 }
 
@@ -4321,7 +4314,7 @@ int ndpi_finalize_initialization(struct ndpi_detection_module_struct *ndpi_str) 
   if(ndpi_str->finalized) /* Already finalized */
     return 0;
 
-  // load_string_based_protocols(ndpi_str);
+  load_string_based_protocols(ndpi_str);
 
   if(dissectors_init(ndpi_str)) {
     NDPI_LOG_ERR(ndpi_str, "Error dissectors_init\n");
