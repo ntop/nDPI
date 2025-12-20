c992de9532383f062ab79c1c1dc3d22f2734ee6e
Luca Deri <deri@ntop.org>
2025-12-20 16:03:09 +0100

    Increased check value

--- src/include/ndpi_typedefs.h
+++ src/include/ndpi_typedefs.h
@@ -1943,11 +1943,11 @@ struct ndpi_flow_struct {
 
 #if !defined(NDPI_CFFI_PREPROCESSING) && defined(__linux__)
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
-_Static_assert(sizeof(((struct ndpi_flow_struct *)0)->protos) <= 264,
-               "Size of the struct member protocols increased to more than 264 bytes, "
+_Static_assert(sizeof(((struct ndpi_flow_struct *)0)->protos) <= 328,
+               "Size of the struct member protocols increased to more than 328 bytes, "
                "please check if this change is necessary.");
-_Static_assert(sizeof(struct ndpi_flow_struct) <= 1240,
-               "Size of the flow struct increased to more than 1240 bytes, "
+_Static_assert(sizeof(struct ndpi_flow_struct) <= 1304,
+               "Size of the flow struct increased to more than 1304 bytes, "
                "please check if this change is necessary.");
 #endif
 #endif
