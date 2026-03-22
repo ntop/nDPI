8058406da5f4e85f0c2547b47191487a3147f1c9
Luca Deri <deri@ntop.org>
2026-03-22 18:02:13 +0100

    Possible fix for python binding configuration issue

--- src/include/ndpi_typedefs.h
+++ src/include/ndpi_typedefs.h
@@ -853,14 +853,13 @@ typedef enum {
   tls_heartbeat,
 } ndpi_tls_block_type;
 
-PACK_ON
 struct ndpi_tls_block {
   u_int8_t block_type /* ndpi_tls_block_type */;
   int16_t len; /* + = src->dst, - = dst->src */
   /* Optional, leave it at the end */
   u_int8_t same_pkt:1, _unused:7;
   u_int16_t msec_delta; /* Used to store protocol_id in ja4 hash */
-} PACK_OFF;
+};
 
 struct ndpi_flow_tcp_struct {
   struct {
