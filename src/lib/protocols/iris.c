e7802cd81a1d92333db397d7a2803f445a7b9442
Ivan Nardi <nardi.ivan@gmail.com>
2026-05-07 21:08:15 +0200

    iris: another attempt to fix endianess

--- src/lib/protocols/iris.c
+++ src/lib/protocols/iris.c
@@ -123,10 +123,10 @@ static void ndpi_check_iris(struct ndpi_detection_module_struct *ndpi_struct,
 
     struct ndpi_packet_struct *packet = &ndpi_struct->packet;
 
-    uint32_t message_length = 0U;
+    uint16_t message_length = 0U;
     uint16_t message_type_or_error_code = 0U;
 
-    message_length = le32toh(*(uint32_t *)packet->payload);
+    message_length = le16toh(*(uint16_t *)packet->payload);
     memcpy(&message_type_or_error_code, packet->payload + sizeof(uint32_t) * 3, sizeof(uint16_t));
 
     switch (message_type_or_error_code)
