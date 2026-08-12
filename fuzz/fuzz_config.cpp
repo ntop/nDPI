f896c71a4cf3d4ab33aa8799f54dec3eb0209794
Ivan Nardi <nardi.ivan@gmail.com>
2026-08-12 12:34:18 +0200

    Fix compilation of fuzzing

--- fuzz/fuzz_config.cpp
+++ fuzz/fuzz_config.cpp
@@ -56,7 +56,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   else
     g_ctx = NULL;
 
-  ndpi_info_mod = ndpi_init_detection_module(g_ctx);
+  ndpi_info_mod = ndpi_init_detection_module(g_ctx, NDPI_LICENSE_NON_COMMERCIAL_LGPL);
 
   set_ndpi_debug_function(ndpi_info_mod, NULL);
 
